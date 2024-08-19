9e28a51b773a9e6e5b3f3c453b08a8eb10609336
tyler <tyler.reitz@protonmail.com>
2024-08-19 22:35:01 +0000

    Part 1 Completed

--- project/app/config.py
+++ project/app/config.py
@@ -1,6 +1,7 @@
 import logging
 from functools import lru_cache
 
+from pydantic import AnyUrl
 from pydantic_settings import BaseSettings
 
 
@@ -10,6 +11,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
   environment: str = "dev"
   testing: bool = bool(0)
+  database_url: AnyUrl = None
 
 @lru_cache()
 def get_settings() -> BaseSettings:
