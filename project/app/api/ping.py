1c0a847bd5789ac677298dbf86d456633ca3f86d
Tyler Reitz <tyler.reitz@EVITECORP-M-WS29.attlocal.net>
2024-08-22 13:22:34 -0700

    test release

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -8,7 +8,7 @@ router = APIRouter()
 @router.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {
-        "ping": "pong!!!",
+        "ping": "PONG",
         "environment": settings.environment,
         "testing": settings.testing,
     }
