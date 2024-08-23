8d90945ce9020999de9d9fb2708d4bcc2c142ae2
Tyler Reitz <tyler.reitz@EVITECORP-M-WS29.attlocal.net>
2024-08-23 11:48:29 -0700

    run isort

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -3,11 +3,9 @@ from typing import List
 from fastapi import APIRouter, HTTPException, Path
 
 from app.api import crud
-from app.models.pydantic import (
-    SummaryPayloadSchema,  # isort skip
-    SummaryResponseSchema,
-    SummaryUpdatePayloadSchema,
-)
+from app.models.pydantic import SummaryPayloadSchema  # isort skip
+from app.models.pydantic import (SummaryResponseSchema,
+                                 SummaryUpdatePayloadSchema)
 from app.models.tortoise import SummarySchema
 
 router = APIRouter()
