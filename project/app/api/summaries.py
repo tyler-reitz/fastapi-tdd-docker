4e9d2bcdcfa56e42d72c29caf6eda63c326661ff
Tyler Reitz <tyler.reitz@EVITECORP-M-WS29.attlocal.net>
2024-08-23 12:01:01 -0700

    isort fix redux

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -3,10 +3,13 @@ from typing import List
 from fastapi import APIRouter, HTTPException, Path
 
 from app.api import crud
-from app.models.pydantic import (SummaryPayloadSchema,  # isort skip
-                                 SummaryResponseSchema,
-                                 SummaryUpdatePayloadSchema)
 from app.models.tortoise import SummarySchema
+
+from app.models.pydantic import (  # isort:skip
+    SummaryPayloadSchema,
+    SummaryResponseSchema,
+    SummaryUpdatePayloadSchema,
+)
 
 router = APIRouter()
 
